dd28b41f5493d5ad8ed7a9595bc03e004b7bf345
Sebastian4090 <bastek4090@gmail.com>
2023-09-22 18:27:28 +0200

    Adding new sites

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,14 +9,27 @@ import './App.css';
 function App() {
 
     const [socials, setSocials] = useState([]);
-    const [searchfield, setSearchfield] = useState('')
-    const [count, setCount] = useState(0)
-    
-    useEffect(()=> {
-        fetch('https://my-json-server.typicode.com/Sebastian4090/SocialsDB/Socials')
-        .then(response => response.json())
-        .then(sites => {setSocials(sites)});
-    }, [])
+    const [searchfield, setSearchfield] = useState('');
+
+    useEffect(() => {
+        let sitesArray = [];
+        const urls = ['https://my-json-server.typicode.com/Sebastian4090/SocialsDB/Socials',
+                      'https://my-json-server.typicode.com/Sebastian4090/SocialsDB/Socials2'];
+        
+        Promise.all(urls.map(link => {
+            return fetch(link).then(response => response.json());
+        })).then(sites => {
+            for (let i=0;i<sites[0].length;i++) {
+                sitesArray.push(sites[0][i])
+            }
+            for (let i=0;i<sites[1].length;i++) {
+                sitesArray.push(sites[1][i])
+            }
+
+            setSocials(sitesArray);
+        })
+
+    }, []);
 
     const onSearchChange = (event) => {
         setSearchfield(event.target.value)
@@ -29,7 +42,6 @@ function App() {
         (
             <div className='tc'>
                 <h1 className='f1'>SocialApps</h1>
-                <button onClick={()=>setCount(count+1)}></button>
                 <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
