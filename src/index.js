a9c72560e4f8203a07ad910f167ab0e82130a9c5
Sebastian4090 <bastek4090@gmail.com>
2023-12-28 14:10:11 +0100

    redux fully implemented

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import  createLogger  from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { searchApps } from './reducers';
+import { searchApps, requestApps } from './reducers';
+import { thunk } from 'redux-thunk';
 
 const logger = createLogger;
-const store = createStore(searchApps, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchApps, requestApps })
+const store = 
+  createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
